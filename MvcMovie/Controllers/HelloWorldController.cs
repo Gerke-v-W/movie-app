d7a439a0e7f4f67352acb0ed3231c5603f20a804
Gerke-v-W <S1161016@student.windesheim.nl>
2022-05-13 11:44:49 +0200

    init commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            
+            return View();
+        }
+        
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Test(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, your ID: {ID}");
+        }
+    }
+}
